15eefb8c46aff2e6a3c46273e19cc67d7aadaf4a
imamhossain94 <imamagun94@gmail.com>
2021-07-26 23:50:08 +0600

    feat: calculator button added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function App() {
+
+const App = () => {
+    const [result, setResult] = useState("hii");
     return (
         <>
-           <h1>Hello World</h1> 
+           <div className="container">
+                <form>
+                    <input type="text" value={result}/>
+                </form>
+
+                <div className="keypad">
+                    <button>Clear</button>
+                    <button>C</button>
+                    <button>&divide;</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>&times;</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>&ndash;</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                    <button>0</button>
+                    <button>.</button>
+                    <button>=</button>
+                </div>
+           </div>
         </>
     )
 }
+
+export default App;
